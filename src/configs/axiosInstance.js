edad6cb3631bd9c0a11a12086dcd38673fcae134
puobyt <puobyt@gmail.com>
2025-07-26 02:59:12 +0530

    update api url

--- src/configs/axiosInstance.js
+++ src/configs/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
+  baseURL: 'https://erp-backend-6k0e.onrender.com' ,
   headers: {
     'Content-Type': 'application/json',
   }, 
